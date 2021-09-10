318f5ffa942aff5e4e79435c2b8d78a6b18e45a6
morello <lauramorello1604@gmail.com>
2021-09-10 08:55:37 +0200

    Modification du fichier bienvenue.cpp

---
+++ bienvenue.cpp
@@ -0,0 +1,11 @@
+//MORELLO Laura 10/09/21
+//TODO Indiquer ce que fait le programme
+#include <iostream>
+
+int main()
+{
+   //TODO Afficher un message de bienvenue
+   std::cout << "Hello world" << std::endl;
+   return 0;
+}
+
